c8bce3fd3ed8271f4f50ca2a7fb9fca4c23fe4ed
Nelson Gomes Neto <ngomesneto@outlook.com>
2026-09-21 21:36:11 -0300

    Fix ASCII Image visualizer \o/
    It turns out I was just reading it incorrectly hahaha.
    It's looking pretty good but I forgot almost no fonts are squary, so the image looks very stretched vertically hahaha.
    
    Well, I tried a Minecraft square font and it looks much better. But I still need to add more brightness maps, the deltas are simply too big.

--- ASCII Image [C++]/main.cpp
+++ ASCII Image [C++]/main.cpp
@@ -6,17 +6,14 @@ const std::string_view kResetColor = "\e[0m";
 
 //                             0123456
 const std::string kAsciiMap = " .-*o#@";
-char Ascii(const int brightness) {
-  return kAsciiMap[brightness];
-}
+char Ascii(const int brightness) { return kAsciiMap[brightness]; }
 
 struct Pixel {
   const uint8_t r = 0, g = 0, b = 0;
   Pixel() {}
-  Pixel(const uint8_t r_, const uint8_t g_, const uint8_t b_) : r(r_), g(g_), b(b_) {}
-  int Hex() const {
-    return ((int)r << 16) | ((int)g << 8) | (int) b;
-  }
+  Pixel(const uint8_t r_, const uint8_t g_, const uint8_t b_)
+      : r(r_), g(g_), b(b_) {}
+  int Hex() const { return ((int)r << 16) | ((int)g << 8) | (int)b; }
   std::string AnsiEscapeColor() const {
     return std::format("\e[38;2;{};{};{}m", r, g, b);
   }
@@ -33,11 +30,12 @@ struct std::formatter<Pixel> {
 
 struct Image {
   const std::vector<std::vector<Pixel>> pixels;
-  const int n,   m;
-  Image(const std::vector<std::vector<Pixel>>& pixels_) : pixels(pixels_), n(pixels.size()), m(pixels[0].size()) {}
+  const int n, m;
+  Image(const std::vector<std::vector<Pixel>>& pixels_)
+      : pixels(pixels_), n(pixels.size()), m(pixels[0].size()) {}
 
   static Image BuildImage(const std::string& image_filepath) {
-    std::ifstream fin(image_filepath);
+    std::ifstream fin(image_filepath, std::ios::binary);
     std::string header;
     std::getline(fin, header);
     assert(header == "P6");
@@ -51,24 +49,27 @@ struct Image {
     for (auto [i, line] : pixels | std::views::enumerate) {
       line.reserve(m);
       for (int j = 0; j < m; ++j) {
-        const uint8_t r = raw_pixels[i*m + j], g = raw_pixels[i*m + j + 1], b = raw_pixels[i*m + j + 2];
+        const int px_pos = (i * m + j) * 3;
+        const uint8_t r = raw_pixels[px_pos], g = raw_pixels[px_pos + 1],
+                      b = raw_pixels[px_pos + 2];
         line.emplace_back(r, g, b);
       }
     }
-    std::println("Built {} x {} image | pixels[100][:4]: {}", n, m, pixels[100] | std::views::take(5));
+    std::println("Built {} x {} image | pixels[0][:4]: {}", n, m,
+                 pixels[0] | std::views::take(5));
     return Image(pixels);
   }
 
   void PrintAscii(const int letter_size) const {
-    const double ratio = (double) 1 / letter_size;
+    const double ratio = (double)1 / letter_size;
     const int an = std::ceil(n * ratio), am = std::ceil(m * ratio);
     std::println("original size: {} x {}", n, m);
-    std::println("letter_size: {} {} -> {} x {}", letter_size, ratio, an, am);  
+    std::println("letter_size: {} {} -> {} x {}", letter_size, ratio, an, am);
 
     const int area = letter_size * letter_size;
     // std::print("{}{}{}", kClearScreen, kMoveTo00, kResetColor);
-    for (int i = 0; i + letter_size - 1 < n; i += letter_size) {
-      for (int j = 0; j + letter_size - 1 < m; j += letter_size) {
+    for (int i = 0; i < n; i += letter_size) {
+      for (int j = 0; j < m; j += letter_size) {
         int r = 0, g = 0, b = 0;
         for (int bi = 0; bi < letter_size && i + bi < n; ++bi) {
           for (int bj = 0; bj < letter_size && j + bj < m; ++bj) {
@@ -82,8 +83,10 @@ struct Image {
         assert(g >= 0 && g < 256);
         assert(b >= 0 && b < 256);
         const int brightness = (r * 3 + g * 5 + b * 2) / 255 / 2;
+        // const int brightness = (r * 2 + g * 8 + b * 2) / 255 / 2;
         assert(brightness < kAsciiMap.size());
         const Pixel pixel(r, g, b);
+        // Try switching to painting the background on empty space instead!
         std::print("{}{}", pixel.AnsiEscapeColor(), Ascii(brightness));
       }
       std::println("");
